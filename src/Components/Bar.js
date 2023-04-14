f63363a6909dfd4ef97c825d7effc3df0cc044b6
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-14 19:00:54 +0530

    functionality added with rest buitton

--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -1,31 +1,45 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const Bar = () => {
-    const [progress,setProgress] = useState(0)
-    const[isRunning,setIsRunning ] = useState(false)
+  const [progress, setProgress] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
-    useEffect(()=>{
-if(progress < 100 && isRunning){
-      setTimeout(()=>
-        setProgress(prev => prev += 1),50)
-}
-    },[progress,isRunning])
+
+  const handleProgress = () => {
+   
+    if(progress == 100){
+        setProgress(0)
+        setIsRunning(false)
+    }
+  };
+
+  useEffect(() => {
+    if (progress < 100 && isRunning) {
+      setTimeout(() => setProgress((prev) => (prev += 1)), 50);
+    }
+  }, [progress, isRunning]);
   return (
-    <div className='container'>
-    <h1>Progress Bar</h1>
-    <div className="progressbar">
-        <div style={{
+    <div className="container">
+      <h1>Progress Bar</h1>
+      <div className="progressbar">
+        <div
+          style={{
             height: "100%",
             width: `${progress}%`,
             backgroundColor: "#a66cff",
-            transition:"width 0.5s"
-        }}></div>
-        <span className="progressPercent">{progress }%</span>
+            transition: "width 0.5s",
+          }}
+        ></div>
+        <span className="progressPercent">{progress}%</span>
+      </div>
+      <div className="button">
+      <button className="btn" onClick={()=>setIsRunning(!isRunning)}>
+        Run
+      </button>
+      <button className="btn" onClick={handleProgress}>Reset</button>
+      </div>
     </div>
-    <button className="btn" onClick={() => {setIsRunning(true)}}>Run</button>
-    {/* <button className="btn" onClick={() => {setProgress(0)}}>Reset</button> */}
-</div>
-  )
-}
+  );
+};
 
-export default Bar
+export default Bar;
